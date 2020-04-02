d214dec5fe60785c3ad377dc663d78acb3b484fe
sch <2524356811@qq.com>
2020-04-02 17:27:27 +0800

    init

---
+++ src/main/java/com/sch/springboot/controller/HtmlToHtml.java
@@ -0,0 +1,22 @@
+package com.sch.springboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/*
+ *  @Author SunChanHui
+ *  @Create Time 2020-04-02
+ * @PackName com.sch.springboot.controller
+ * @Project springboot
+ */
+
+/**
+ * @author sch
+ */
+@Controller
+public class HtmlToHtml {
+    @RequestMapping(path = "success")
+    public String success(){
+        return "success";
+    }
+}
--- src/main/java/com/sch/springboot/controller/SayHello.java
+++ src/main/java/com/sch/springboot/controller/SayHello.java
@@ -6,17 +6,36 @@ package com.sch.springboot.controller;
  * @Project springboot
  */
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author sch
  * @RestController === @Controller+@ResponseBody
  */
-@RestController
+@Controller
 public class SayHello {
+    @ResponseBody
     @RequestMapping(path = "/hello")
     public String sayHello(){
         return "Hello SpringBoot By Quickly Start!";
+    }
+
+    @RequestMapping(path = "/thymeleaf")
+    public ModelAndView thymeleafStudy(){
+        ModelAndView mav=new ModelAndView("thymeleaf");
+        List<Integer> list=new ArrayList<>();
+        list.add(2);
+        list.add(8);
+        list.add(6);
+        list.add(1);
+        list.add(2);
+        mav.addObject("list",list);
+        return mav;
     }
 }
